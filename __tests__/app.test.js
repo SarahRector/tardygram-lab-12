ebc2c9603dc70b15b43d360223685224f0ea7b47
SarahRector <rector.sarah.r@gmail.com>
2020-09-28 14:51:01 -0700

    added post route for user creation and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,23 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/user-service');
 
 describe('tardygram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('lets a user sign up via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'sarah@rector.com',
+        password: 'wordpass'
+      });
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'sarah@rector.com'
+    });
   });
 });
